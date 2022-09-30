877c751a84092214cf0b7f0b96483a83b2ef59a1
rika kaminoyama <richun.06@gmail.com>
2022-09-30 22:49:42 +0900

    ツイート作成画面の実装

---
+++ db/schema.rb
@@ -0,0 +1,13 @@
+
+ActiveRecord::Schema.define(version: 2022_09_30_132220) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
